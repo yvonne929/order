7d85cbc1f5041cd9ea0ebb0f853cad597051d68b
yvonne929 <yvonne20030929@gmail.com>
2022-04-29 16:04:58 +0800

    Testing

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -3,9 +3,14 @@ package com.fju.order;
 public class Order {
     int amount;
     Delivery delivery;
+    int shipFeed;
 
     public Order(int amount, Delivery delivery) {
         this.amount = amount;
         this.delivery = delivery;
+    }
+
+    public int total(){
+        return amount+shipFeed;
     }
 }
